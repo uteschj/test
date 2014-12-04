8000e24fb0d7be902d29c050327fb6ae8ee3df57
uteschj <uteschj@gmail.com>
2014-12-03 22:59:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -221,11 +221,11 @@ alert(data);
     break;
 
     case 'error':
-        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+        alert('<li>ERROR -> MSG:' + e.msg + '</li>');
     break;
 
     default:
-        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+        alert('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
     break;
   }
 }
