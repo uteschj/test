405d3a64748a97de35df12f266e0514613d7a012
uteschj <uteschj@gmail.com>
2014-12-03 18:12:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ else
     }
 
     function fail(error) {
-        alert("An error has occurred: Code = " = error.code);
+        alert("An error has occurred: Code = " + error.code);
     }
     
     function checkPreAuth() {
